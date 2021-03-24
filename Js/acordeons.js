a06dcf5db7e5c3f9cfc9d896cd5f15e80c0b803a
Juan Ignacio <>
2021-03-24 15:37:06 -0600

    Agrego el codigo completo de mobile homepage

---
+++ Js/acordeons.js
@@ -0,0 +1,14 @@
+const btnItems = document.querySelector('.footer__wrapper--acordeon');
+
+function removeClass(e) {
+  e.target.classList.remove('active');
+}
+
+btnItems.addEventListener('click', (e) => {
+  if (e.target.className === 'item__boton active') {
+    removeClass(e);
+  } else {
+    removeClass(e);
+    e.target.classList.add('active');
+  }
+});
