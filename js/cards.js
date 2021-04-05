12a9ef4e9dbc9ce334c158192c1163c5c8a1e426
JeampauÃl Salazar Castillo <jpaulsalazar28@gmail.com>
2021-04-04 20:05:23 -0600

    se agrego el codigo del carrusel

--- js/cards.js
+++ js/cards.js
@@ -1,12 +1,20 @@
 const url = 'https://60414895f34cf600173c9bb5.mockapi.io/api/product';
 const cardsContainer = document.getElementById('cards-scroll');
+const btnNext = document.querySelector('.btn__next--js');
+const btnPrev = document.querySelector('.btn__prev--js');
 
+btnNext.addEventListener('click', (e) => {
+    cardsContainer.scrollLeft += 150;
+})
+btnPrev.addEventListener('click', (e) => {
+    cardsContainer.scrollLeft -= 150;
+})
 function createCards(product) {
   for (let i = 0; i < 8; i += 1) {
     const htmlCards = `
-    <div class="product__card">
+    <div class="product__card" id="product__card--js">
       <div class= "product__card--image">
-        <img src="${product[i].large}" alt="${product[i].name}">
+        <img src="${product[i].large}" alt="">
       </div>
       <div class="product__card--info">
         <p class="product__card--title">${product[i].name}</p>
