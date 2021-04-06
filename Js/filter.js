cf8a4cf65e974eac5a88cabf1d0e37952275eca4
mariaconejo <mary03081@gmail.com>
2021-04-06 14:54:21 -0600

    Se arreglo conflictos

--- Js/filter.js
+++ Js/filter.js
@@ -1,6 +1,5 @@
 const url = 'https://60414895f34cf600173c9bb5.mockapi.io/api/product';
 const cardsContainer = document.getElementById('cards-shop');
-<<<<<<< HEAD
 const optionsCategories = document.querySelector('#categories--js');
 const optionsPrices = document.querySelector('#prices--js');
 
@@ -29,9 +28,7 @@ function createCards(product) {
     cardsContainer.innerHTML += htmlCards;
   }
 }
-=======
 const checkboxes = document.querySelectorAll(".filter__item--content input[type=checkbox]");
->>>>>>> modal
 
 function filterbyCategories(data) {
   const listaProductos = data;
@@ -63,7 +60,6 @@ optionsPrices.addEventListener('change', () =>{
     });
   }
 })
-<<<<<<< HEAD
 
 function ApiData() {
   fetch(url, {
@@ -72,55 +68,8 @@ function ApiData() {
     .then((response) => response.json())
     .then((data) => {
       filterbyCategories(data);
-      filterByPrices(data);
     })
     .catch(() => {
     });
 }
 ApiData();
-
-=======
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    const listaProductos = data;
-    if(checkboxes.checked){
-      const filteredContent = listaProductos.filter((Producto) => Producto.category.toLowerCase().includes(`${checkboxes.value}`));
-      if (filteredContent) {
-        createCards(filteredContent);
-      }
-    }else{
-      createCards(data);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-  function createCards(product) {
-    for (let i = 0; i < 10; i += 1) {
-      const htmlCards = `
-      <div data-id="${product[i].id}" class="product__card--container">
-        <div class="product__card--shop">
-          <div class= "product__card--medium--image">
-            <img src="${product[i].medium}" alt="">
-          </div>
-          <div class= "product__card--large--image">
-            <img src="${product[i].large}" alt="">
-          </div>
-          <div class="product__card--info--shop">
-          <p class="product__card--name--shop">${product[i].name}</p>
-          <p class="product__card--price--shop">¢${product[i].price}</p>
-          </div>
-          <div class="product__card--button--shop">
-            <a data-id="${product[i].id}" class="anchor__button anchor__button--secondary anchor__button--small .js-add__drawer'" href="#">Agregar al carrito</a>
-          </div>
-        </div>
-      </div>
-
-      `;
-      cardsContainer.innerHTML += htmlCards;
-    }
-  }
->>>>>>> modal
