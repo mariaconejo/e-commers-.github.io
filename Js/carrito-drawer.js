dae2042026f287bea3dd8a0fcdefb7ad740625d6
Juan Ignacio <>
2021-04-07 02:12:26 -0600

    Agrego js del carrito

--- Js/carrito-drawer.js
+++ Js/carrito-drawer.js
@@ -8,7 +8,14 @@ const burger = document.getElementById('open-menu');
 const drawerCollapse = document.getElementById('drawer-box');
 const urlCart = 'https://60414895f34cf600173c9bb5.mockapi.io/api/productosdelcarrito';
 const listContainer = document.getElementById('drawer-product');
+const test = document.getElementById('cards-scroll');
+const alert = document.getElementById('alert');
+
 let list = [];
+
+function getListElement(id) {
+  return document.querySelector(`div[data-id="${id}"]`);
+}
 
 function openCart() {
   drawerCollapse.style.right = '0px';
@@ -53,58 +60,113 @@ btnDrawer.addEventListener('click', () => {
   closeCart();
 });
 
-function itemDrawer(product) {
+listContainer.addEventListener('click',(e)=>{
+  let trashButton = document.querySelectorAll('.close__button--js');
+  console.log(trashButton);
+  trashButton.forEach(button => {
+    console.log(trashButton);
+    if(e.target === button){
+      deleteProduct(e.target.dataset.id)
+    }
+  });
+})
+function deleteProduct(id){
+  fetch(`${urlCart}/${id}`, {
+    method: "DELETE"
+  })
+    .then((response) => {
+      if (response.ok) {
+        const liEliminado = getListElement(id);
+        liEliminado.remove();
+      } else {
+        throw new Error(response.status);
+      }
+    })
+    .catch((err) => {
+      alert(`Ocurrió un error de tipo ${err}`);
+    });
+}
+
+test.addEventListener('click',(e) =>{
+  const buybutton = document.querySelectorAll('.add__button--js');
+  buybutton.forEach(button => {
+    if(e.target === button){
+      let obj = {
+        'name': `${e.target.dataset.name}`,
+        'price':`${e.target.dataset.price}`,
+        'image': `${e.target.dataset.img}`
+      }
+      addProduct(obj)
+    }
+  });
+})
+
+
+function insertProduct(product){
   return `
-    <div data-id="${product.id}">
-      <div "class="drawer__item">
-        <div class="drawer__item--image">
-          <img src="${product.medium}" alt="">
-        </div>
-        <div>
-          <p>${product.name}</p>
-          <p>¢${product.price} </p>
-        </div>
-        <div>
-          <button data-id="${product.id} id="delete-item"><img src="./img/close-icon.svg" alt="Cerrar-menu"></button>
-        </div>
+  <div class="drawer__item--container--js">
+    <div class="drawer__item" data-id="${product.id}">
+      <div class="drawer__item--image">
+        <img src="${product.image}" alt="">
+      </div>
+      <div>
+        <p>${product.name}</p>
+        <p>¢${product.price} </p>
+      </div>
+      <div>
+        <button class= "close__button--js" data-id="${product.id}" id="delete-item"><img class="close__button--img" src="./img/close-icon.svg" alt="Cerrar-menu"></button>
       </div>
     </div>
-  `;
+  </div>
+  `
 }
 
-function productCreation(nameProduct, priceProduct , imageProduct, idProduct){
-  let idProduct = {
-    id: idProduct,
-    name:nameProduct,
-    price:priceProduct,
-    image: imageProduct
-  };
-}
-
-
-
-fetch(urlCart, {
-  method: 'GET',
-})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-  fetch(urlCart, {
-    method: 'POST',
+function addProduct(obj){
+  fetch(`${urlCart}`, {
+    method: "POST",
+    body: JSON.stringify(obj),
+    headers: {
+      "Content-Type": "application/json"
+    },
   })
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      createAlert(data);
+      listContainer.innerHTML += insertProduct(data);
     })
-    .catch((err) => {
-      console.error(err);
-    });
+}
+
+
+function createAlert(data){
+  const alertHtml = `
+  <h3 class= "alert__title">Felicidades</h3>
+  <p class= "alert__text">${data.name} Se agrego satisfactoriamente</p>
+  <button class= "alert__button">Ok</button>
+  `
+  alert.innerHTML = alertHtml;
+}
+
+function getProductHtmlList(products) {
+  return products
+    .map((product) => {
+      return  insertProduct(product)
+    })
+    .join("");
+}
+
+fetch(urlCart, {
+  method: "GET"
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((products) => {
+    const htmlListItems = getProductHtmlList(products);
+    listContainer.innerHTML = htmlListItems;
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
--- js/cards.js
+++ js/cards.js
@@ -12,7 +12,7 @@ btnPrev.addEventListener('click', () => {
 function createCards(product) {
   for (let i = 0; i < 8; i += 1) {
     const htmlCards = `
-    <div data-id="${product[i].id}" class="product__card" id="product__card--js">
+    <div data-id="${product[i].id}" class="product__card">
       <div class= "product__card--image">
         <img src="${product[i].large}" alt="">
       </div>
@@ -24,8 +24,8 @@ function createCards(product) {
         <a data-id="${product[i].id}" class="anchor__button anchor__button--secondary anchor__button--medium" href="#">Más información</a>
       </div>
       <div class="product__card--button">
-      <button data-id="${product[i].id}" class="anchor__button anchor__button--principal anchor__button--medium button__shop " href="#">Agregar al carrito</button>
-    </div>
+        <button data-img="${product[i].small}"  data-name="${product[i].name}" data-price="${product[i].price}" class="anchor__button anchor__button--principal anchor__button--medium button__shop add__button--js " href="#">Agregar al carrito</button>
+      </div>
     </div>
     `;
     cardsContainer.innerHTML += htmlCards;
