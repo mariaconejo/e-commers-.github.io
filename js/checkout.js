85cb54f2ede35bd3ff62dc965f8733d2599c762a
Kendoll Castro M <kendollcastrom.@MacBook-Pro-de-Kendoll.local>
2021-04-09 16:26:37 -0600

    Agregue nueva estructura al documento y agregue varios estilos de del chekout

--- js/checkout.js
+++ js/checkout.js
@@ -5,7 +5,6 @@ const prevBtnSec = document.querySelector('.checkout__previuos--btn--1');
 const nextBtnSec = document.querySelector('.checkout__next--btn--1');
 const prevBtnThird = document.querySelector('.checkout__previuos--btn--2');
 const nextBtnThird = document.querySelector('.checkout__next--btn--2');
-const submitBtn = document.querySelector('.submit');
 const progressText = document.querySelectorAll('.checkout__step--js p');
 const progressCheck = document.querySelectorAll('.checkout__step--js .check');
 const bullet = document.querySelectorAll('.checkout__step--js .checkout__bullet--js');
@@ -19,54 +18,36 @@ nextBtnFirst.addEventListener('click', (event) => {
   progressText[current - 1].classList.add('active');
   current += 1;
 });
-nextBtnSec.addEventListener("click", function(event){
+nextBtnSec.addEventListener('click', (event) => {
   event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
+  slidePage.style.marginLeft = '-50%';
+  bullet[current - 1].classList.add('active');
+  progressCheck[current - 1].classList.add('active');
+  progressText[current - 1].classList.add('active');
   current += 1;
 });
-nextBtnThird.addEventListener("click", function(event){
+nextBtnThird.addEventListener('click', (event) => {
   event.preventDefault();
-  slidePage.style.marginLeft = "-75%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
+  slidePage.style.marginLeft = '-75%';
+  bullet[current - 1].classList.add('active');
+  progressCheck[current - 1].classList.add('active');
+  progressText[current - 1].classList.add('active');
   current += 1;
 });
-submitBtn.addEventListener("click", function(){
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-  setTimeout(function(){
-    alert("Your Form Successfully Signed up");
-    location.reload();
-  },800);
-});
-
-prevBtnSec.addEventListener("click", function(event){
+prevBtnSec.addEventListener('click', (event) => {
   event.preventDefault();
-  slidePage.style.marginLeft = "0%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
+  slidePage.style.marginLeft = '0%';
+  bullet[current - 2].classList.remove('active');
+  progressCheck[current - 2].classList.remove('active');
+  progressText[current - 2].classList.remove('active');
   current -= 1;
 });
-prevBtnThird.addEventListener("click", function(event){
+prevBtnThird.addEventListener('click', (event) => {
   event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
+  slidePage.style.marginLeft = '-25%';
+  bullet[current - 2].classList.remove('active');
+  progressCheck[current - 2].classList.remove('active');
+  progressText[current - 2].classList.remove('active');
   current -= 1;
 });
-prevBtnFourth.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
+// eslint-disable-next-line no-undef
